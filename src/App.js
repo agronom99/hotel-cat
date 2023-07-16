f91f518248bc5aee2748c8660c139a7baab0289a
Vitalii MV <agronom9913@gmail.com>
2023-07-16 17:24:39 +0300

    2023.07.16 17-24

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-// import UncontrolledExample from "./components/Carousel";
-// import UncontrolledExampleReviews from "./components/CarouselReviews";
-// import Footer from "./components/Footer";
+import UncontrolledExample from "./components/Carousel";
+import UncontrolledExampleReviews from "./components/CarouselReviews";
+import Footer from "./components/Footer";
 // import Header from "./components/Header";
-// import ColorSchemesExample from "./components/Header2";
-// import KitchenSinkExample from "./components/MapAddress";
+import ColorSchemesExample from "./components/Header2";
+import KitchenSinkExample from "./components/MapAddress";
 
 import SectionHome from "./components/Section__home";
 import SectionConditions from "./components/Section_conditions";
@@ -12,14 +12,14 @@ import "./styles/App.scss";
 function App() {
   return (
     <div>
-      {/* <ColorSchemesExample /> */}
+      <ColorSchemesExample />
       <SectionHome />
       <SectionConditions />
-      {/* <UncontrolledExample /> */}
-      {/* <UncontrolledExampleReviews /> */}
-      {/* <KitchenSinkExample /> */}
-      {/* <ColorSchemesExample /> */}
-      {/* <Footer /> */}
+      <UncontrolledExample />
+      <UncontrolledExampleReviews />
+      <KitchenSinkExample />
+      <ColorSchemesExample />
+      <Footer />
     </div>
   );
 }
